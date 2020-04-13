2327660d857d5aeaf9c12bc3ef33235a11398475
DELTAJoSch <deltajosch@gmail.com>
2020-04-13 22:23:12 +0200

    added tutorial project

---
+++ EasyMVVM_Tutorial/Models/CounterModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyMVVM_Tutorial.Models
+{
+    internal class CounterModel
+    {
+        private ulong _counterValue;
+
+        public async Task<ulong> IncreaseCounter()
+        {
+            return _counterValue++;
+        }
+
+        public async Task ResetCounter()
+        {
+            _counterValue = 0;
+        }
+    }
+}
